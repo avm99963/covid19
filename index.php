068b43e06b21e6ea455c07c5998e6e30f2c5f7be
avm99963 <me@avm99963.com>
2020-07-08 15:45:18 +0200

    Add disclaimer about the data
    Also, this commit adds a box-shadow to the key.
    
    Change-Id: Iccd5e843080b14db1c19f18e9b6c3f00848e9c10

--- index.php
+++ index.php
@@ -11,6 +11,26 @@
       padding-bottom: 200px;
     }
 
+    .note {
+      width: calc(100% - 32px);
+      padding: 16px;
+      margin-bottom: 16px;
+      border-radius: 4px;
+      background: #FFEB3B;
+      box-shadow: 0 1px 3px 0px rgba(0, 0, 0, .6);
+
+      font-size: 13px;
+      line-height: 1.4;
+    }
+
+    .note p:first-child {
+      margin-top: 0;
+    }
+
+    .note p:last-child {
+      margin-bottom: 0;
+    }
+
     .graphs {
       max-width: 100%;
     }
@@ -24,6 +44,7 @@
 
       background: white;
       border-radius: 4px;
+      box-shadow: 0 1px 3px 0px rgba(0, 0, 0, .3);
     }
 
     .key-container h3 {
@@ -61,6 +82,12 @@
     </style>
   </head>
   <body>
+    <div class="note">
+      <p><b>Nota</b>: La implementació que s'ha realitzat per calcular les dades podria ser incorrecta ja que no ha estat revisada per cap tercer, així que les gràfiques d'aquesta pàgina podrien ser incorrectes.
+      <p>Les dades s'obtenen diretament de l'API de la Generalitat de Catalunya, però <a href="https://biocomsc.upc.edu/en/shared/delays_spain_30062020.pdf">segons el grup BIOCOMSC de la UPC</a>, les dades corresponents a l'última setmana no són fiables. Tot i així, es continuen graficant en aquesta pàgina totes les dades disponibles per tal de donar una fita inferior dels valors finals en les dades de l'última setmana.</p>
+      <p>Es pot trobar el codi font complet del càlcul i generació de les gràfiques <a href="https://gerrit.avm99963.com/plugins/gitiles/covid19/+/refs/heads/master">aquí</a>.</p>
+    </div>
+
     <img class="graphs" src="output/graph.svg">
 
     <div class="key-container">
